0bc52f3e09617731f24c659588fa0f00ab926c36
cskrub33 <cskrub33@naver.com>
2020-04-25 12:51:06 +0900

    test

---
+++ src/test.java
@@ -0,0 +1,23 @@
+
+public class test {
+	
+	public static void main(String[] args)
+	{
+		
+		int[]  arr= new int[] {1,2,3,4};
+		int[] arr1 = {1,2,3,4};
+		int[] arr2 = new int[100];
+		
+		
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+		
+		
+	
+		
+	}
+	
+
+}
